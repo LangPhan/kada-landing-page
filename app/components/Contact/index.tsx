d0dab3048da4c7568a09e7423b40b90935599493
LangPhan <langphankk@gmail.com>
2023-11-18 08:08:01 +0700

    Change Contact

--- app/components/Contact/index.tsx
+++ app/components/Contact/index.tsx
@@ -9,17 +9,7 @@ export default function Contact() {
     2xl:pb-24 2xl:before:-top-[2.4375rem] 2xl:before:h-10
     2xl:before:rounded-t-[2.5rem] 2xl:after:-bottom-[2.4375rem] 2xl:after:h-10 2xl:after:rounded-b-[2.5rem]"
     >
-      <div className="px-0 pb-[2.6875rem] sm:pt-[3.1875rem] md:px-0 md:py-12 lg:px-0 lg:pb-8 lg:pt-4 2xl:pb-[2.875rem] 2xl:pt-12">
-        <div className="relative mx-0 my-auto max-w-[23.4375rem] md:max-w-[46.875rem] lg:max-w-none lg:px-0 lg:py-[15.37%] xl:ml-[1/2] xl:w-[90rem] xl:max-w-none xl:-translate-x-[50%] xl:p-0">
-          <div className="grid-contact grid-contact-res grid grid-cols-[repeat(2,1fr)]">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <Form />
-          </div>
-        </div>
-      </div>
+      <div className="px-0 pb-[2.6875rem] sm:pt-[3.1875rem] md:px-0 md:py-12 lg:px-0 lg:pb-8 lg:pt-4 2xl:pb-[2.875rem] 2xl:pt-12"></div>
     </section>
   );
 }
