f83da77295587d9706be9ad77d4072553001e1a8
LangPhan <langphankk@gmail.com>
2023-12-30 01:51:14 +0700

    Refactor variable names and improve code readability

--- app/database.ts
+++ app/database.ts
@@ -4,6 +4,5 @@ const databases = new Databases(client);
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("658e89514f94ab5dda0f");
-client.setOrigin('https://www.kada.vn');
+  .setProject("658e89514f94ab5dda0f")
 export default databases
