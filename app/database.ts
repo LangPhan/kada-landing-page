e6f267ac14c672ae3b6b01696689b0b22065f121
LangPhan <81663477+LangPhan@users.noreply.github.com>
2023-12-30 01:37:52 +0700

    Update database.ts

--- app/database.ts
+++ app/database.ts
@@ -5,5 +5,5 @@ const databases = new Databases(client);
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("658e89514f94ab5dda0f");
-
+client.setOrigin('https://www.kada.vn');
 export default databases
